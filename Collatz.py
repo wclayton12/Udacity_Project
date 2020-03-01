194e90e854dfec1da1903630c7cbb00ed2b55078
William Clayton <williamclayton36@gmail.com>
2020-02-29 19:26:52 -0500

    increased interactiion between user program with additional print statements

--- Collatz.py
+++ Collatz.py
@@ -1,5 +1,5 @@
 ####The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows: start with any positive integer n. Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term.
-If## if the previous term is odd, the next term is 3 times the previous term plus 1### The conjecture is that no matter what value of n, the sequence will always reach 1. 
+## if the previous term is odd, the next term is 3 times the previous term plus 1### The conjecture is that no matter what value of n, the sequence will always reach 1. 
 def is_even(number):
     return number %  2 ==   0
 def check_number(number):
@@ -11,7 +11,7 @@ def check_number(number):
         collatz(number)
 def collatz(number):
     if number != 1:
-        print(number)
+        print("You have selected: ", number)
         check_number(number)
     if number == 1:
         print("Final number is: "  +  str(number))
